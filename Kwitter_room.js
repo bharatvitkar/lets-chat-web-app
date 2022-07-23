a4a64e31e8f38f806024422196e34048311926b1
bharatvitkar <79301656+bharatvitkar@users.noreply.github.com>
2022-07-23 09:48:05 +0530

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,13 @@
+//ADD YOUR FIREBASE LINKS HERE
+var firebaseConfig = {
+    apiKey: "AIzaSyD7L-c5uZhSbtKaHZKieTgZ_EWXLuhA_DA",
+    authDomain: "kwitter-project-class.firebaseapp.com",
+    databaseURL: "https://kwitter-project-class-default-rtdb.firebaseio.com",
+    projectId: "kwitter-project-class",
+    storageBucket: "kwitter-project-class.appspot.com",
+    messagingSenderId: "205964313747",
+    appId: "1:205964313747:web:cd06443289df4f670db966"
+  };
+  
+  // Initialize Firebase
+   firebase.initializeApp(firebaseConfig);
